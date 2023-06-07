f9b6232b27a6a907136dd01674ace4d181ea12a5
beuleu <billyklotz789@gmail.com>
2023-06-07 11:24:01 +0200

    user model fix

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-      name             : { type: String, trim: true },
-      age             : { type: String, trim: true },
-      email            : { type: String, required: true, lowercase: true, unique: true },
-      password         : { type: String, required: true },
-    },
-    { timestamps: true })
+      name : { type: String, trim: true },
+      age  : { type: String, trim: true },
+      email: { type: String, required: true, lowercase: true, unique: true },
+      date : { type: Date, required: true, default: Date.now() },
+    }
+)
 
 module.exports = mongoose.model('User', userSchema);
